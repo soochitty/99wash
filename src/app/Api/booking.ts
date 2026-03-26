5c6be2468d99c24ee461b4d72dc0cca1d9a8db39
soochitty <chittysouju0510@gmail.com>
2026-03-26 19:03:34 +0530

    updated the url

--- src/app/Api/booking.ts
+++ src/app/Api/booking.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class Booking {
-  private apiUrl = "https://99wash-backend-c94z.vercel.app/api/booking.js"; // backend URL
+  private apiUrl = "https://99wash-backend.vercel.app/api/booking.js"; // backend URL
 
   constructor(public http: HttpClient) {}
 
